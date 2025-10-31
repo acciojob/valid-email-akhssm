087ce93399f9ea5a3f0e0afa225c50c416bb44b5
Akshay <152252912+akhssm@users.noreply.github.com>
2025-10-31 11:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+  if (!str) return false; 
+
+  const regex = /^[\w.-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+  return regex.test(str) && !str.includes("..") && !str.includes("--") && !str.includes(".@") && !str.includes("@.");
 }
 
 // Do not change the code below.
